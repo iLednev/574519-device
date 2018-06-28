b85bb3cde88c0326813a416bb7aa9d6de617ac9e
iLednev <ilednevii@gmail.com>
2018-06-28 11:40:33 +0300

    Начало js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//document - вся страница
+//window - всё окно
+//document.querySelector("селектор") {} - поиск по css-селектору
+//element.addEventListener("действие", function () {}) - перехват пользовательского действия (клик, нажатие клавиши и т.д.)
+//preventDefault() - отмена стандартного действия элемента
+//element.classList.add("класс") - добавление класса элементу
+
